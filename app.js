f4c411a4e713e6fc1bbd180a5512934ef863d48c
Alexander <alexander.gotra@gmail.com>
2022-02-07 13:18:32 +0200

    updated env.example

--- app.js
+++ app.js
@@ -5,36 +5,24 @@ const transactionsRouter = require('./routes/api/transactions')
 
 require('dotenv').config()
 
-const { DB_HOST } = process.env
+const app = express()
+app.use(cors())
+app.use(express.json())
+
+app.use('/', transactionsRouter)
+
+const { DB_HOST, PORT = 3000 } = process.env
 
 mongoose
   .connect(DB_HOST)
-  .then(() => console.log('BitLab Database connected'))
+  .then(() => {
+    app.listen(PORT)
+    console.log('BitLab Database connected')
+    console.log('Server running on port 3000')
+  })
   .catch(err => {
     console.log(err.message)
     process.exit(1)
   })
-
-const app = express()
-app.use(cors())
-
-app.get('/', transactionsRouter)
-
-app.listen(3000, () => console.log('Server running on port 3000'))
-
-// app.use('/api/transactions', transactionsRouter)
-
-// app.use((req, res) => {
-//   res.status(404).json({ message: 'Not found' })
-// })
-
-// app.use((err, req, res, next) => {
-//   const { status = 500, message = 'Server error' } = err
-//   res.status(status).json({
-//     status: 'error',
-//     code: status,
-//     message,
-//   })
-// })
 
 module.exports = app
--- controllers/getTransactionsController.js
+++ controllers/getTransactionsController.js
@@ -1,36 +1,5 @@
 const getTransactions = async (req, res) => {
-  const result = [
-    {
-      blockHash:
-        '0x2d45b3877ee0d7e9f0ab6db53e309d3ae203a28040bd86ca72135e758a011289',
-      blockNumber: '0x1b4145',
-      from: '0x2a65aca4d5fc5b5c859090a6c34d164135398226',
-      gas: '0x15f90',
-      gasPrice: '0x4a817c800',
-      hash: '0x5c74bfa06191a63c6453b426a14bb81f43ad78af4df1cae0af44b7d0de5426c4',
-      input: '0x',
-      nonce: '0xd8d81',
-      to: '0x466d2200fefb6c74da79c877435d436160ce2686',
-      transactionIndex: '0x0',
-      value: '0x1126d7a952e58c00',
-      type: '0x0',
-    },
-    {
-      blockHash:
-        '0x2d45b3877ee0d7e9f0ab6db53e309d3ae203a28040bd86ca72135e758a011289',
-      blockNumber: '0x1b4145',
-      from: '0x2a65aca4d5fc5b5c859090a6c34d164135398226',
-      gas: '0x15f90',
-      gasPrice: '0x4a817c800',
-      hash: '0xf4dca00b764f971c5df117a835921938aa9769439afe1c639c4cea703021ec03',
-      input: '0x',
-      nonce: '0xd8d82',
-      to: '0xe39234e9664540059460b57222cdbe3cd6da2690',
-      transactionIndex: '0x1',
-      value: '0xff895bc1f056800',
-      type: '0x0',
-    },
-  ]
+  // const result =
 
   res.status(200).json({
     status: 'success',
