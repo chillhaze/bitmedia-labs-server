f3ce40f9e7eaf0c888af2339cf60bb43df7f4beb
Alexander <alexander.gotra@gmail.com>
2022-02-13 20:09:10 +0200

    added aggregation

--- controllers/getTransactions.js
+++ controllers/getTransactions.js
@@ -14,11 +14,28 @@ const getTransactions = async (req, res) => {
   const count = transactionsCount - 1000
 
   // Get all Transactions and sort. allowDiskUse working only with paid cluster
-  const transactions = await Transaction.find({}, '', {
-    skip,
-    limit: Number(pageItemsLimit),
-  }).sort({ createdAt: -1 })
-  // .allowDiskUse(true)
+  // const transactions = await Transaction.find({}, '', {
+  //   skip,
+  //   limit: Number(pageItemsLimit),
+  // }).sort({ createdAt: -1 })
+  // // .allowDiskUse(true)
+
+  const transactions = await Transaction.aggregate([
+    {
+      $match: {},
+    },
+    {
+      $skip: skip,
+    },
+    {
+      $limit: Number(pageItemsLimit),
+    },
+    {
+      $sort: {
+        createdAt: -1,
+      },
+    },
+  ])
 
   if (!transactions) {
     const error = new Error(`Transactions not found.`)
